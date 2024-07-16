31aea16821ad9d4fc994f8bbb04ca05b78b690cf
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-07-16 14:56:23 +0600

    added hamburger responsive navbar

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector(".menu").addEventListener("click", () =>{
+    document.querySelector(".nav-items").style.left = "0"
+})
+document.querySelector(".close").addEventListener("click", () =>{
+    document.querySelector(".nav-items").style.left = "-100%"
+})
